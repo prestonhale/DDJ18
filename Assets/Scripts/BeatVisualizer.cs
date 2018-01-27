85389688b6ea20c53fab8d7e023fe40501e8666d
Charlie Tran <hi@charlietran.com>
2018-01-27 12:20:21 -0500

    Add Music Manager

---
+++ Assets/Scripts/BeatVisualizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatVisualizer : MonoBehaviour
+{
+    Color currentColor;
+    Renderer cubeRenderer;
+
+    void Start()
+    {
+        Music.Instance.beatDetector.OnBeat += OnBeatDetected;
+        Music.Instance.Play();
+        currentColor = Color.black;
+        cubeRenderer = GetComponent<Renderer>();
+    }
+
+    void Update() {
+        currentColor = Color.Lerp(currentColor, Color.black, Time.deltaTime);
+        cubeRenderer.material.color = currentColor;
+    }
+
+    void OnBeatDetected() {
+        currentColor = Color.red;
+    }
+}
---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public float timeSinceLastBeat = 0f;
+    public float timeOfLastBeat = 0f;
+    public AudioClip musicClip;
+    public SimpleBeatDetection beatDetector;
+
+    Camera _camera;
+    AudioSource _audioSource;
+
+    public static Music Instance;
+
+    void Awake()
+    {
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        _camera = Camera.main;
+        if (_camera != null)
+        {
+            _audioSource = _camera.GetComponent<AudioSource>();
+            beatDetector = _camera.GetComponent<SimpleBeatDetection>();
+        }
+        if (_audioSource != null)
+        {
+            _audioSource.clip = musicClip;
+            _audioSource.playOnAwake = false;
+        }
+        if (beatDetector != null)
+        {
+            _audioSource.clip = musicClip;
+            beatDetector.audioSource = _audioSource;
+            beatDetector.bufferSize = 1024;
+            beatDetector.OnBeat += OnBeatDetected;
+        }
+        timeSinceLastBeat = 0f;
+        timeOfLastBeat = 0f;
+    }
+
+    void Update() {
+        timeSinceLastBeat = Time.time - timeOfLastBeat;
+    }
+
+    void OnBeatDetected()
+    {
+        timeOfLastBeat = Time.time;
+    }
+
+    public void Play()
+    {
+        _audioSource.Play();
+    }
+}
