e8376c73ed680af6fe9283ba9a14a9d324dd9564
Charlie Tran <hi@charlietran.com>
2018-01-28 13:55:11 -0500

    Add Menu scene

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Canvas introCanvas;
+    public Canvas instructionsCanvas;
+
+    public Text promptText;
+
+    bool instructionsShowing = false;
+
+    void Start()
+    {
+        StartCoroutine("BlinkPrompt");
+        introCanvas.gameObject.SetActive(true);
+        instructionsCanvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (instructionsShowing)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Warehouse - Preston");
+            }
+            else
+            {
+                introCanvas.gameObject.SetActive(false);
+                instructionsCanvas.gameObject.SetActive(true);
+                StopCoroutine("BlinkPrompt");
+                promptText.gameObject.SetActive(false);
+                instructionsShowing = true;
+            }
+        }
+    }
+
+    IEnumerator BlinkPrompt()
+    {
+        bool showing = false;
+        promptText.enabled = false;
+
+        while (true)
+        {
+            showing = !showing;
+            promptText.enabled = showing;
+            yield return new WaitForSeconds(0.8f);
+        }
+
+    }
+}
